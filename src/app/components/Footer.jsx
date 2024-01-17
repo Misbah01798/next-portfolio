27892c8ec447d3c403e64cc2a84f7a4e2e2cac2a
Misbah01798 <misbahuddin01798@gmail.com>
2024-01-17 10:53:59 +0600

    footer img tag solve

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Footer = () => {
@@ -5,7 +6,7 @@ const Footer = () => {
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         <span><div className="w-16 rounded-full">
-            <img src="/images/Misbah.png" className="w-16 rounded-full"/>
+            <Image src="/images/Misbah.png" alt="mis" width={50} height={50} className="w-16 rounded-full"/>
           </div></span>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
